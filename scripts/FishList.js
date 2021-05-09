fbae7bc49aca172fdc46e04f827bbe86f965eff7
Andrew Webb <webba@ihopu.org>
2021-05-08 20:55:36 -0500

    used javascript to display fishes database on to the DOM

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,12 +1,12 @@
 // Import the function that returns a copy of the fish array
-import {getFish} from ''
+import {getFish} from "./database.js"
 
 export const FishList = () => {
     // Invoke the function that you imported from the database module
     const fishes = getFish()
 
     // Start building a string filled with HTML syntax
-    const htmlString = '<article class="fishList">'
+    let htmlString = '<article class="fishList">'
 
     // Create HTNL representations of each fish here
     for (const fish of fishes) {
@@ -19,8 +19,7 @@ export const FishList = () => {
             <div class="fish__length">${fish.length}</div>
             <div class="fish__location">${fish.location}</div>
             <div class="fish__diet">${fish.food}</div>
-        </section>
-`
+        </section>`
     }
     htmlString += `</article>`
 
